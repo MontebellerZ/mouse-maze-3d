70ee839e5b5ff83792947c6d4350d8504389a1a3
MontebellerZ <54958016+MontebellerZ@users.noreply.github.com>
2023-11-22 17:55:56 +0000

    trying to give textures

--- index.js
+++ index.js
@@ -17,128 +17,142 @@ const WALL_MATERIAL = new THREE.MeshStandardMaterial({ color: 0xfafafa }); // Gr
 const FLOOR_MATERIAL = new THREE.MeshStandardMaterial({ color: 0xc9a771, side: THREE.DoubleSide }); // Yellowwish color for floors
 
 function render() {
-    requestAnimationFrame(() => {
-        renderer.render(scene, camera);
-        render();
-    });
+	requestAnimationFrame(() => {
+		renderer.render(scene, camera);
+		render();
+	});
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function setScene(centerCoord) {
-    scene = new THREE.Scene();
+	scene = new THREE.Scene();
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-    document.body.appendChild(renderer.domElement);
+	document.body.appendChild(renderer.domElement);
 
-    window.addEventListener("resize", onWindowResize, false);
+	window.addEventListener("resize", onWindowResize, false);
 
-    const fov = 90;
-    const aspect = window.innerWidth / window.innerHeight;
-    const near = 0.1;
-    const far = 1000.0;
+	const fov = 90;
+	const aspect = window.innerWidth / window.innerHeight;
+	const near = 0.1;
+	const far = 1000.0;
 
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(centerCoord, 8, centerCoord);
+	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.set(centerCoord, 8, centerCoord);
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(centerCoord, 0, centerCoord);
-    controls.update();
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.target.set(centerCoord, 0, centerCoord);
+	controls.update();
 }
 
 function setWall(width, depth, x, z, material = null) {
-    const geometry = new THREE.BoxGeometry(width, wallHeight, depth); // Adjust dimensions as needed
-    const wall = new THREE.Mesh(geometry, material || WALL_MATERIAL);
-    wall.position.set(x, wallHeight / 2, z); // Position walls according to maze coordinates
-    wall.castShadow = true;
-    wall.receiveShadow = true;
-    scene.add(wall);
+	const geometry = new THREE.BoxGeometry(width, wallHeight, depth); // Adjust dimensions as needed
+	const wall = new THREE.Mesh(geometry, material || WALL_MATERIAL);
+	wall.position.set(x, wallHeight / 2, z); // Position walls according to maze coordinates
+	wall.castShadow = true;
+	wall.receiveShadow = true;
+	scene.add(wall);
 }
 
 function setMaze(maze) {
-    // Loop through maze cells to create walls
-    maze.reverse().forEach((row, z) => {
-        row.forEach((cell, x) => {
-            // Check each wall of the cell and create a wall if it exists
-            if (cell.top) setWall(wallThick, wallThin, x, z + wallThick / 2 - wallThin / 2);
+	// Loop through maze cells to create walls
+	maze.reverse().forEach((row, z) => {
+		row.forEach((cell, x) => {
+			// Check each wall of the cell and create a wall if it exists
+			if (cell.top) setWall(wallThick, wallThin, x, z + wallThick / 2 - wallThin / 2);
 
-            if (cell.bottom) setWall(wallThick, wallThin, x, z - wallThick / 2 + wallThin / 2);
+			if (cell.bottom) setWall(wallThick, wallThin, x, z - wallThick / 2 + wallThin / 2);
 
-            if (cell.left) setWall(wallThin, wallThick, x - wallThick / 2 + wallThin / 2, z);
+			if (cell.left) setWall(wallThin, wallThick, x - wallThick / 2 + wallThin / 2, z);
 
-            if (cell.right) setWall(wallThin, wallThick, x + wallThick / 2 - wallThin / 2, z);
-        });
-    });
+			if (cell.right) setWall(wallThin, wallThick, x + wallThick / 2 - wallThin / 2, z);
+		});
+	});
 }
 
 function setLights() {
-    const light = new THREE.DirectionalLight(0xffffff, 1.0);
-    light.position.set(20, 30, 10);
-    light.target.position.set(0, 0, 0);
-    light.castShadow = true;
-    light.shadow.bias = -0.0002;
-    light.shadow.mapSize.width = 8192;
-    light.shadow.mapSize.height = 8192;
-    light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 500.0;
-    light.shadow.camera.near = 0.5;
-    light.shadow.camera.far = 500.0;
-    light.shadow.camera.left = 100;
-    light.shadow.camera.right = -100;
-    light.shadow.camera.top = 100;
-    light.shadow.camera.bottom = -100;
-    scene.add(light);
+	const light = new THREE.DirectionalLight(0xffffff, 1.0);
+	light.position.set(20, 30, 10);
+	light.target.position.set(0, 0, 0);
+	light.castShadow = true;
+	light.shadow.bias = -0.0002;
+	light.shadow.mapSize.width = 8192;
+	light.shadow.mapSize.height = 8192;
+	light.shadow.camera.near = 0.1;
+	light.shadow.camera.far = 500.0;
+	light.shadow.camera.near = 0.5;
+	light.shadow.camera.far = 500.0;
+	light.shadow.camera.left = 100;
+	light.shadow.camera.right = -100;
+	light.shadow.camera.top = 100;
+	light.shadow.camera.bottom = -100;
+	scene.add(light);
 
-    const ambientLight = new THREE.AmbientLight(0x404040);
-    scene.add(ambientLight);
+	const ambientLight = new THREE.AmbientLight(0x404040);
+	scene.add(ambientLight);
 }
 
 function setFloors(dificulty, center, material) {
-    const width = dificulty + 3;
-    const geometry = new THREE.PlaneGeometry(width, width, 10, 10);
-    const plane = new THREE.Mesh(geometry, material || FLOOR_MATERIAL);
-    plane.castShadow = false;
-    plane.receiveShadow = true;
-    plane.rotation.x = -Math.PI / 2;
-    plane.position.set(center, 0, center);
-    scene.add(plane);
+	const width = dificulty + 3;
+	// const geometry = new THREE.PlaneGeometry(width, width, 10, 10);
+	// const plane = new THREE.Mesh(geometry, material || FLOOR_MATERIAL);
+	// plane.castShadow = false;
+	// plane.receiveShadow = true;
+	// plane.rotation.x = -Math.PI / 2;
+	// plane.position.set(center, 0, center);
+	// scene.add(plane);
+
+	var groundTexture = new THREE.TextureLoader().load("./resources/textures/floor1.jpg");
+	groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+	groundTexture.repeat.set(10000, 10000);
+	groundTexture.anisotropy = 16;
+	groundTexture.encoding = THREE.SRGBColorSpace;
+
+	var groundMaterial = new THREE.MeshStandardMaterial({ map: groundTexture });
+
+	var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(10000, 10000), groundMaterial);
+	mesh.position.y = 0.0;
+	mesh.rotation.x = -Math.PI / 2;
+	mesh.receiveShadow = true;
+	scene.add(mesh);
 }
 
 function setBackground() {
-    const loader = new THREE.CubeTextureLoader();
-    const texture = loader.load([
-        "./resources/skybox/posx.jpg",
-        "./resources/skybox/negx.jpg",
-        "./resources/skybox/posy.jpg",
-        "./resources/skybox/negy.jpg",
-        "./resources/skybox/posz.jpg",
-        "./resources/skybox/negz.jpg",
-    ]);
-    scene.background = texture;
+	const loader = new THREE.CubeTextureLoader();
+	const texture = loader.load([
+		"./resources/skybox/posx.jpg",
+		"./resources/skybox/negx.jpg",
+		"./resources/skybox/posy.jpg",
+		"./resources/skybox/negy.jpg",
+		"./resources/skybox/posz.jpg",
+		"./resources/skybox/negz.jpg",
+	]);
+	scene.background = texture;
 }
 
 function init() {
-    const dificulty = 10;
-    const centerCoord = (dificulty - 1) / 2;
-    const maze = generateMaze(dificulty, dificulty);
+	const dificulty = 10;
+	const centerCoord = (dificulty - 1) / 2;
+	const maze = generateMaze(dificulty, dificulty);
 
-    setScene(centerCoord);
+	setScene(centerCoord);
 
-    setBackground();
-    setFloors(dificulty, centerCoord);
-    setMaze(maze);
-    setLights();
+	setBackground();
+	setFloors(dificulty, centerCoord);
+	setMaze(maze);
+	setLights();
 
-    render();
+	render();
 }
 
 window.onload = init;
